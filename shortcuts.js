4a778a525a3f42d27bf33ce657e32b5e2c18fa54
sbkwgh <sbkwgh@gmail.com>
2016-05-30 18:08:11 +0100

    New replacement format, plus google translate replacement

--- shortcuts.js
+++ shortcuts.js
@@ -1,46 +1,50 @@
 module.exports = {
 	'!b': {
 		site: 'bing',
-		expansion: 'http://www.bing.com/search?q=__QUERY__'
+		expansion: 'http://www.bing.com/search?q={query}'
 	},
 	'!w': {
 		site: 'wikipedia',
-		expansion: 'https://en.wikipedia.org/w/index.php?search=__QUERY__'
+		expansion: 'https://en.wikipedia.org/w/index.php?search={query}'
 	},
 	'!so' : {
 		site: 'stackoverflow',
-		expansion: 'http://stackoverflow.com/search?q=__QUERY__'
+		expansion: 'http://stackoverflow.com/search?q={query}'
 	},
 	'!ddg': {
 		site: 'duck duck go',
-		expansion: 'https://duckduckgo.com/?q=__QUERY__'
+		expansion: 'https://duckduckgo.com/?q={query}'
 	},
 	'!gi': {
 		site: 'google images',
-		expansion: 'https://encrypted.google.com/search?q=__QUERY__&tbm=isch'
+		expansion: 'https://encrypted.google.com/search?q={query}&tbm=isch'
 	},
 	'!wkt': {
 		site: 'wiktionary',
-		expansion: 'https://en.wiktionary.org/w/index.php?search=__QUERY__&title=Special%3ASearch'
+		expansion: 'https://en.wiktionary.org/w/index.php?search={query}&title=Special%3ASearch'
 	},
 	'!bbc': {
 		site: 'bbc.co.uk',
-		expansion: 'http://www.bbc.co.uk/search?q=__QUERY__'
+		expansion: 'http://www.bbc.co.uk/search?q={query}'
 	},
 	'!yt': {
 		site: 'YouTube',
-		expansion: 'https://www.youtube.com/results?search_query=__QUERY__'
+		expansion: 'https://www.youtube.com/results?search_query={query}'
 	},
 	'!yh': {
 		site: 'Yahoo',
-		expansion: 'https://search.yahoo.com/search?p=__QUERY__'
+		expansion: 'https://search.yahoo.com/search?p={query}'
 	},
 	'!a': {
 		site: 'Amazon.com',
-		expansion: 'https://www.amazon.com/s/?field-keywords=__QUERY__'
+		expansion: 'https://www.amazon.com/s/?field-keywords={query}'
 	},
 	'!tw': {
 		site: 'Twitter',
-		expansion: 'https://twitter.com/search?q=__QUERY__&src=typd'
+		expansion: 'https://twitter.com/search?q={query}&src=typd'
+	},
+	'!gt': {
+		site: 'Google Translate',
+		expansion:'https://translate.google.com/#{1}/{2}/{query}'
 	}
 };
